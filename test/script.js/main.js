fa554eb7470ddf0a0e35616dcec47bb55f89f689
Lee Yul <pklo41221@gmail.com>
2022-07-07 19:34:23 +0900

    0707

---
+++ test/script.js/main.js
@@ -0,0 +1,6 @@
+const init = ()=>{
+    $(`header`).load(`/test/fragment/header.html`)
+    $(`nav`).load(`/test/fragment/nav.html`)
+    $(`aside`).load(`/test/fragment/aside.html`)
+    $(`footer`).load(`/test/fragment/footer.html`)
+}
